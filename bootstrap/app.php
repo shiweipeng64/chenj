b66f4b36cb1d641d495ded390740c84fbc7c8d61
hang.lu <luhang@webuildus.com>
2018-07-10 22:29:46 +0800

    test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,7 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
-var_dump(realpath(__DIR__));
+var_dump(file_exists(realpath(__DIR__.'/../config')));
 exit;
 $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
